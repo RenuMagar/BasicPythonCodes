4f04e9cfb7a0272a230c81d0a098e80f50e0e543
RenuMagar <145140786+RenuMagar@users.noreply.github.com>
2023-09-16 20:34:29 +0530

    Create CheckrectangleisSquare.py

---
+++ CheckrectangleisSquare.py
@@ -0,0 +1,7 @@
+#take length and breadth of a rectangle from user and check if it is square ore not
+length=input("enter a length")
+breadth=int(input("enter a breadth"))
+if length==breadth:
+    print("it is square")
+else:
+    print("it is only rectangle")
